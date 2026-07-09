1a1a9b79d1821e157746022c370369f10697f0e6
bharathautomation <139088966+bharathtechacademy@users.noreply.github.com>
2026-07-09 10:47:24 +0530

    api

---
+++ documentation/api-testing.md
@@ -0,0 +1,38 @@
+## API Testing - Step By Step
+
+## What is API? 
+API (meaning application programming interface) Is a combination of a set of routines, protocols, and supporting tools used in the background to create a connection between your application UI and the backend server to exchange information. 
+
+
+## What is the difference between an API web service and a microservice? 
+
+## Web Service 
+API runs over the internet or web, called web services. 
+
+All web services are APIs, but all APIs are not web services. 
+
+
+## Microservice
+Microservice is a tiny API that is useful for exchanging information between the components inside the application. 
+
+## What is API testing? Why is it important? 
+API testing is a type of testing technique that involves testing the APIs directly without using application UI. 
+
+## Benefits of API Testing 
+1. Early Issue Detection 
+2. Faster execution compared to UI. 
+3. Broader test coverage compared to UI 
+4. Completely independent from the UI changes 
+5. API Testing is automation-friendly. 
+
+
+## As there are multiple benefits, we have, so only API testing is enough to release the product. ?
+NO
+
+Using API testing, we can validate the core business logic, but that is not enough to release the application to the customer. 
+
+## Popular API architectures ?
+
+## REST
+## SOAP
+## GRAPHQL
--- playwright-tdd-framework/tests/ui/ui-tests.spec.ts
+++ playwright-tdd-framework/tests/ui/ui-tests.spec.ts
@@ -20,93 +20,93 @@ test.describe('Creatio CRM UI Tests', () => {
     });
 
 
-    // //Test Case 1: Verify Cookies pop-up is displayed. 
-    // test('Verify Cookies pop-up is displayed', async () => {
-    //     await loginPage.launchApplication();
-    //     await cookiesPage.verifyCookiesPopUpIsDisplayed();
-    // });
+    //Test Case 1: Verify Cookies pop-up is displayed. 
+    test('Verify Cookies pop-up is displayed', async () => {
+        await loginPage.launchApplication();
+        await cookiesPage.verifyCookiesPopUpIsDisplayed();
+    });
 
-    // //Test Case 2: Verify Cookies pop-up content.
-    // test('Verify Cookies popup content', async () => {
-    //     testData = data["Verify Cookies popup content"];
-    //     await loginPage.launchApplication();
-    //     await cookiesPage.verifyCookiesPopUpIsDisplayed();
-    //     await cookiesPage.verifyCookiesPopUpContent(testData["content"]);
-    // });
+    //Test Case 2: Verify Cookies pop-up content.
+    test('Verify Cookies popup content', async () => {
+        testData = data["Verify Cookies popup content"];
+        await loginPage.launchApplication();
+        await cookiesPage.verifyCookiesPopUpIsDisplayed();
+        await cookiesPage.verifyCookiesPopUpContent(testData["content"]);
+    });
 
-    // //Test Case 3: Verify Logos are displayed in Cookies pop-up.
-    // test('Verify Logos are displayed in Cookies pop-up', async () => {
-    //     await loginPage.launchApplication();
-    //     await cookiesPage.verifyCookiesPopUpIsDisplayed();
-    //     await cookiesPage.verifyLogosDisplayedInCookiesPopUp();
-    // });
+    //Test Case 3: Verify Logos are displayed in Cookies pop-up.
+    test('Verify Logos are displayed in Cookies pop-up', async () => {
+        await loginPage.launchApplication();
+        await cookiesPage.verifyCookiesPopUpIsDisplayed();
+        await cookiesPage.verifyLogosDisplayedInCookiesPopUp();
+    });
 
-    // //Test Case 4: Verify Switch buttons are displayed in Cookies pop-up.
-    // test('Verify Switch buttons are displayed in Cookies pop-up', async () => {
-    //     await loginPage.launchApplication();
-    //     await cookiesPage.verifyCookiesPopUpIsDisplayed();
-    //     await cookiesPage.verifySwitchButtonsDisplayedInCookiesPopUp();
-    // });
+    //Test Case 4: Verify Switch buttons are displayed in Cookies pop-up.
+    test('Verify Switch buttons are displayed in Cookies pop-up', async () => {
+        await loginPage.launchApplication();
+        await cookiesPage.verifyCookiesPopUpIsDisplayed();
+        await cookiesPage.verifySwitchButtonsDisplayedInCookiesPopUp();
+    });
 
-    // //Test Case 5: Verify Selection buttons are displayed in Cookies pop-up.
-    // test('Verify Selection buttons are displayed in Cookies pop-up', async () => {
-    //     await loginPage.launchApplication();
-    //     await cookiesPage.verifyCookiesPopUpIsDisplayed();
-    //     await cookiesPage.verifySelectionButtonsDisplayedInCookiesPopUp();
-    // });
+    //Test Case 5: Verify Selection buttons are displayed in Cookies pop-up.
+    test('Verify Selection buttons are displayed in Cookies pop-up', async () => {
+        await loginPage.launchApplication();
+        await cookiesPage.verifyCookiesPopUpIsDisplayed();
+        await cookiesPage.verifySelectionButtonsDisplayedInCookiesPopUp();
+    });
 
-    // //Test Case 6: Verify Show-details link is displayed in Cookies pop-up.
-    // test('Verify Show-details link is displayed in Cookies pop-up', async () => {
-    //     await loginPage.launchApplication();
-    //     await cookiesPage.verifyCookiesPopUpIsDisplayed();
-    //     await cookiesPage.verifyShowDetailsLinkDisplayedInCookiesPopUp();
-    // });
+    //Test Case 6: Verify Show-details link is displayed in Cookies pop-up.
+    test('Verify Show-details link is displayed in Cookies pop-up', async () => {
+        await loginPage.launchApplication();
+        await cookiesPage.verifyCookiesPopUpIsDisplayed();
+        await cookiesPage.verifyShowDetailsLinkDisplayedInCookiesPopUp();
+    });
 
-    // //Test Case 7: Verify cookies pop-up is disappeared after clicking on Allow All button.
-    // test('Verify cookies pop-up is disappeared after clicking on Allow All button', async () => {
-    //     await loginPage.launchApplication();
-    //     await cookiesPage.verifyCookiesPopUpIsDisplayed();
-    //     await cookiesPage.verifySelectionButtonsDisplayedInCookiesPopUp();
-    //     await cookiesPage.clickOnSelectionButton('Allow All');
-    //     await cookiesPage.verifyCookiesPopUpIsDisappeared();
-    // });
+    //Test Case 7: Verify cookies pop-up is disappeared after clicking on Allow All button.
+    test('Verify cookies pop-up is disappeared after clicking on Allow All button', async () => {
+        await loginPage.launchApplication();
+        await cookiesPage.verifyCookiesPopUpIsDisplayed();
+        await cookiesPage.verifySelectionButtonsDisplayedInCookiesPopUp();
+        await cookiesPage.clickOnSelectionButton('Allow All');
+        await cookiesPage.verifyCookiesPopUpIsDisappeared();
+    });
 
-    // //Test Case 8: Verify show-details link
-    // test('Verify show-details link is displayed in Cookies pop-up', async () => {
-    //     await loginPage.launchApplication();
+    //Test Case 8: Verify show-details link
+    test('Verify show-details link is displayed in Cookies pop-up', async () => {
+        await loginPage.launchApplication();
 
-    //     await cookiesPage.verifyShowDetailsLinkDisplayedInCookiesPopUp();
-    //     await cookiesPage.clickOnShowDetailsLink();
-    //     await cookiesPage.verifyExpandedViewOfCookiesPopUpIsDisplayed();
-    // });
+        await cookiesPage.verifyShowDetailsLinkDisplayedInCookiesPopUp();
+        await cookiesPage.clickOnShowDetailsLink();
+        await cookiesPage.verifyExpandedViewOfCookiesPopUpIsDisplayed();
+    });
 
-    // //Test Case 9: Verify login functionality with valid credentials.
-    // test('Verify login functionality with valid credentials', async ({ }, testInfo: TestInfo) => {
-    //     testData = data[testInfo.title as keyof typeof data];
-    //     await loginPage.launchApplication();
-    //     await cookiesPage.verifyCookiesPopUpIsDisplayed();
-    //     await cookiesPage.verifySelectionButtonsDisplayedInCookiesPopUp();
-    //     await cookiesPage.clickOnSelectionButton('Allow All');
-    //     await cookiesPage.verifyCookiesPopUpIsDisappeared();
-    //     await loginPage.verifyLoginPageIsDisplayed();
-    //     await loginPage.enterCredentials(testData["username"], testData["password"]);
-    //     await loginPage.clickLoginButton();
-    //     await homePage.verifyHomePageDisplayed();
-    // });
+    //Test Case 9: Verify login functionality with valid credentials.
+    test('Verify login functionality with valid credentials', async ({ }, testInfo: TestInfo) => {
+        testData = data[testInfo.title as keyof typeof data];
+        await loginPage.launchApplication();
+        await cookiesPage.verifyCookiesPopUpIsDisplayed();
+        await cookiesPage.verifySelectionButtonsDisplayedInCookiesPopUp();
+        await cookiesPage.clickOnSelectionButton('Allow All');
+        await cookiesPage.verifyCookiesPopUpIsDisappeared();
+        await loginPage.verifyLoginPageIsDisplayed();
+        await loginPage.enterCredentials(testData["username"], testData["password"]);
+        await loginPage.clickLoginButton();
+        await homePage.verifyHomePageDisplayed();
+    });
 
-    // //Test Case 10: Verify login functionality with invalid credentials.
-    // test('Verify login functionality with invalid credentials', async ({ }, testInfo: TestInfo) => {
-    //     testData = data[testInfo.title as keyof typeof data];
-    //     await loginPage.launchApplication();
-    //     await cookiesPage.verifyCookiesPopUpIsDisplayed();
-    //     await cookiesPage.verifySelectionButtonsDisplayedInCookiesPopUp();
-    //     await cookiesPage.clickOnSelectionButton('Allow All');
-    //     await cookiesPage.verifyCookiesPopUpIsDisappeared();
-    //     await loginPage.verifyLoginPageIsDisplayed();
-    //     await loginPage.enterCredentials(testData["username"], testData["password"]);
-    //     await loginPage.clickLoginButton();
-    //     await loginPage.verifyLoginErrorMessageIsDisplayed();
-    // });
+    //Test Case 10: Verify login functionality with invalid credentials.
+    test('Verify login functionality with invalid credentials', async ({ }, testInfo: TestInfo) => {
+        testData = data[testInfo.title as keyof typeof data];
+        await loginPage.launchApplication();
+        await cookiesPage.verifyCookiesPopUpIsDisplayed();
+        await cookiesPage.verifySelectionButtonsDisplayedInCookiesPopUp();
+        await cookiesPage.clickOnSelectionButton('Allow All');
+        await cookiesPage.verifyCookiesPopUpIsDisappeared();
+        await loginPage.verifyLoginPageIsDisplayed();
+        await loginPage.enterCredentials(testData["username"], testData["password"]);
+        await loginPage.clickLoginButton();
+        await loginPage.verifyLoginErrorMessageIsDisplayed();
+    });
 
     //Test Case 11: Verify logout functionality with valid credentials.
     test('Verify logout functionality with valid credentials', async ({ }, testInfo: TestInfo) => {
